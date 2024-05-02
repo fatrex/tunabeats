76cbd7c2d3c6cb51c82c7aba4ec2e3ba7029a42e
Daniele Lenares <daniele@lenares.me>
2024-05-02 16:01:24 +0200

    feat: add base sidebar

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,16 @@
+<nav class="list-nav">
+  <ul>
+    <li>
+      <a href="/elements/lists">
+        <span class="badge bg-primary-500">(icon)</span>
+        <span class="flex-auto">Albums</span>
+      </a>
+    </li>
+    <li>
+      <a href="/elements/lists">
+        <span class="badge bg-primary-500">(icon)</span>
+        <span class="flex-auto">Artists</span>
+      </a>
+    </li>
+  </ul>
+</nav>
